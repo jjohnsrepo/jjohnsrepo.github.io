dc6d485dab1f762eea464c5d30948cb050014d02
Justin Johnson <90579983+jjohnsrepo@users.noreply.github.com>
2025-03-14 15:02:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var count = 1
 
+function select(input) {
+  if (count % 2 === 0) {
+    input.textContent = "O"
+    input.className = "used2"
+  } else {
+    input.textContent = "X"
+    input.className = "used1"
+  }
+
+  count += 1
+
+  if (count === 10) {
+    alert("The game is over")
+  }
+}
